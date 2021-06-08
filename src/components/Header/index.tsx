0442a5d1f3a6fcefff65a0b343e48a19e5b71735
odairdev <odair.dev@gmail.com>
2021-06-08 10:44:34 -0300

    Logo adjustment

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo_oj.svg'
 import { ModalContext } from '../../ModalContext';
 
 import { Container, Content } from "./styles";
